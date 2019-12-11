64a49e79a103567fe44a5a58dba714d4c7aa2dc9
Robert Hoekstra <robert.hoekstra@hva.nl>
2019-12-11 16:11:05 +0100

    fixed map

--- docs/javascript/app.js
+++ docs/javascript/app.js
@@ -1,10 +1,22 @@
 async function importDataset() {
   const dataset = await d3.dsv(";", "dataset/selectie3.csv");
   const postcodeSet = await d3.dsv(";", "dataset/PC4_BUURTEN.csv");
-  console.log(postcodeSet)
-  console.log(dataset)
+  const sbiCollection = await d3.dsv(";", "dataset/sbi_codes.csv");
+  const test = await d3.json("dataset/PC4_BUURTEN.json");
+  
+  // remember the regular Object? it would convert keys to string
+  // Map keeps the type, so these two are different:
+
+
+  console.log("postcode", postcodeSet);
+  console.log("data UA", dataset);
+  console.log("sbi", sbiCollection);
+  console.log(test)
+
+
 
   const headerNames = d3.keys(dataset[0]);
+
   const week = {
     maandag: headerNames[11],
     dinsdag: headerNames[12],
@@ -14,30 +26,65 @@ async function importDataset() {
     zaterdag: headerNames[16],
     zondag: headerNames[17]
   };
+
+
   console.log(week);
   dataset.forEach(element => {
-    parseInt(element.ma)
-    parseInt(element.di)
-    parseInt(element.woe)
-    parseInt(element.do)
-    parseInt(element.vr)
-    parseInt(element.zat)
-    parseInt(element.zo)
+    parseInt(element.ma);
+    parseInt(element.di);
+    parseInt(element.woe);
+    parseInt(element.do);
+    parseInt(element.vr);
+    parseInt(element.zat);
+    parseInt(element.zo);
 
-    const weekTotaal = 
-    parseInt(element.ma) +
-    parseInt(element.di) +
-    parseInt(element.woe) +
-    parseInt(element.do) +
-    parseInt(element.vr) +
-    parseInt(element.zat) +
-    parseInt(element.zo) 
+    const weekTotaal =
+      parseInt(element.ma) +
+      parseInt(element.di) +
+      parseInt(element.woe) +
+      parseInt(element.do) +
+      parseInt(element.vr) +
+      parseInt(element.zat) +
+      parseInt(element.zo);
 
-    if (Number.isNaN(weekTotaal)){
-      weekTotaal === 1
-      return weekTotaal
+    if (Number.isNaN(weekTotaal)) {
+      weekTotaal === 1;
+      return weekTotaal;
     }
-    console.log(element.ZAAKNAAM + " Verwerkt " + weekTotaal + " zakken afval per week ")
+
+    sbiCollection.forEach(element2 => {
+      if (element.SBI === element2.Code){
+        element.BEDRIJFSTYPE = element2.Betekenis
+      }
+    })
+    // // wat voor soort bedrijf:
+  //  console.log(element.ZAAKNAAM + " Geregistreerd in postcodegebied: " + element.POSTCODE + " " + " Verwerkt " + weekTotaal + " zakken afval per week" + " Het bedrijfstype is " + element.BEDRIJFSTYPE)
   });
+  renderChart(dataset)
 }
+
+
 importDataset();
+
+
+function renderChart(data1, data2, data3) {
+var width = 900;
+var height = 500;
+
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width",width)  // apply width,height to svg
+  .attr("height",height);
+
+var projection = d3.geoMercator();
+var path = d3.geoPath().projection(projection);
+console.log(
+d3.json("dataset/PC4_BUURTEN.json", function(err, geojson) { 
+      projection.fitSize([width,height],geojson); // adjust the projection to the features
+      svg.append("path").attr("d", path(geojson)); // draw the features
+      console.log(data1)
+
+}))}
+
+
+

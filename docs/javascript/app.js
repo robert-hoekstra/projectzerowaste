e600c6c9cdce67a6e60d91814ef7dc1c5fc4af2f
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-01-15 08:56:26 +0100

    Small visual changes

--- docs/javascript/app.js
+++ docs/javascript/app.js
@@ -1,5 +1,5 @@
 async function importDataset() {
-  const dataset = await d3.dsv(";", "dataset/selectie3.csv");
+  const dataset = await d3.dsv(";", "dataset/extraBedrijven.csv");
   const postcodeCollection = await d3.dsv(";", "dataset/PC4_BUURTEN.csv");
   const sbiCollection = await d3.dsv(";", "dataset/sbi_codes.csv");
   const eventCollection = await d3.dsv(";", "dataset/events_amsterdam.csv");
@@ -17,20 +17,27 @@ async function importDataset() {
   console.log("kaart", postcodeMap)
 
 
+
+
+  // test
+
+
+
   const headerNames = d3.keys(dataset[0]);
 
   const week = {
-    maandag: headerNames[11],
-    dinsdag: headerNames[12],
-    woensdag: headerNames[13],
-    donderdag: headerNames[14],
-    vrijdag: headerNames[15],
-    zaterdag: headerNames[16],
-    zondag: headerNames[17]
+    maandag: headerNames[14],
+    dinsdag: headerNames[15],
+    woensdag: headerNames[16],
+    donderdag: headerNames[17],
+    vrijdag: headerNames[18],
+    zaterdag: headerNames[19],
+    zondag: headerNames[20]
   };
 
 
   console.log(week);
+
   dataset.forEach(element => {
     parseInt(element.ma);
     parseInt(element.di);
@@ -49,6 +56,7 @@ async function importDataset() {
       parseInt(element.zat) +
       parseInt(element.zo);
 
+
     if (Number.isNaN(weekTotaal)) {
       weekTotaal === 1;
       return weekTotaal;
@@ -59,6 +67,13 @@ async function importDataset() {
         element.BEDRIJFSTYPE = element2.Betekenis
       }
     })
+    console.log(element.POSTCODE_NR)
+
+
+    // dataset.forEach(element => {
+    //   if (selectedPostalcode === data.)
+    // })
+
     // // wat voor soort bedrijf:
   //  console.log(element.ZAAKNAAM + " Geregistreerd in postcodegebied: " + element.POSTCODE + " " + " Verwerkt " + weekTotaal + " zakken afval per week" + " Het bedrijfstype is " + element.BEDRIJFSTYPE)
   });
@@ -86,7 +101,7 @@ var svg =  d3.select("#chart")
 
 var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
-d3.json("dataset/PC4_BUURTENV2.json", function(err, geojson) { 
+d3.json("dataset/geojson2.json", function(err, geojson) { 
       projection.fitSize([width,height],geojson); // adjust the projection to the features
       // svg.append("path").attr("d", path(geojson)); // draw the 
       
@@ -100,7 +115,7 @@ d3.json("dataset/PC4_BUURTENV2.json", function(err, geojson) {
       .attr("d", path)
       .style("stroke", "#fff")
       .style("stroke-width", "1")
-      .style("fill", "cyan")
+      .style("fill", "red")
       .on("mouseover", function(geojson) {      
         div.transition()        
              .duration(200)      
@@ -119,10 +134,53 @@ d3.json("dataset/PC4_BUURTENV2.json", function(err, geojson) {
              .attr("d", path)
              .style("fill", "red")
              .style("fill-opacity", "1")
+
+             
+
              
         
              
-    })   
+    })
+    .on("click", function(geojson) {
+      let selectedPostalcode = geojson.properties.Postcode4
+      let selectedSurface = geojson.properties.Opp_m2
+
+      function postcodecheck(parameter){
+        if (parameter === dataset.POSTCODE_NR){
+          console.log(dataset.POSTCODE_NR)
+        }
+      }
+
+      postcodecheck(this)
+
+
+      d3.selectAll("path")
+      .classed("selectedPostalcode", false)
+      .attr("d", path)
+      .style("fill-opacity", "0.1")
+      .style("fill", "rgb(91	121	153	)");
+
+
+      d3.select(this)
+      .classed("selectedPostalcode", true)
+      .attr("d", path)
+      .style("fill", "red")
+      .style("fill-opacity", "1");
+
+      d3.select("#selectedArea")
+      .text("Je hebt postcode " + selectedPostalcode + " geselecteerd")
+      d3.select("#postalcode")
+      .text("Postcodegebied: " + selectedPostalcode)
+      d3.select("#surface")
+      .text(selectedSurface + " m2")
+
+      console.log(
+        geojson.properties.postcode4
+      )
+
+
+
+    })
   
       // fade out tooltip on mouse out               
       .on("mouseout", function(geojson) {       
@@ -130,16 +188,13 @@ d3.json("dataset/PC4_BUURTENV2.json", function(err, geojson) {
              .duration(500)      
              .style("opacity", 1);
 
+
           d3.selectAll("path")
           .attr("d", path)
+          .style("fill", "rgb(91	121	153	)")
           .style("fill-opacity", "1")
-             
-          d3.select(this)
-          .attr("d", path)
-          .style("fill", "cyan")
-      });
 
-
+      })
 })}
 
 
